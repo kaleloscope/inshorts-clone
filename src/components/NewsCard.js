f3065f64fcecb66af459c2b6ed2ad53fd9bb6325
Sourabh <kalelsaurabh11@gmail.com>
2021-11-10 16:55:25 +0530

    updated formatted date

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './NewsCard.css';
 
 const NewsCard = ({newsItem}) => {
+
+    const fulldate = new Date(newsItem.publishedAt);
+    const date = fulldate.toString().split(" ");
+    const hour = parseInt(date[4].substring(0,2));
+    const time = hour>12?true:false;
+
     return (
         <div className = "newsCard">
             <img className = "newsImage" alt = {newsItem.title} src = {newsItem.urlToImage ? newsItem.urlToImage:"http://www.aaru.edu.jo/websites/aaru2/wp-content/plugins/learnpress/assets/images/no-image.png?Mobile=1&Source=%2F%5Flayouts%2Fmobile%2Fdispform%2Easpx%3FList%3D78b536db%252De7c7%252D45d9%252Da661%252Ddb2a2aa2fbaf%26View%3D6efc759a%252D0646%252D433c%252Dab6e%252D2f027ffe0799%26RootFolder%3D%252Fwebsites%252Faaru2%252Fwp%252Dcontent%252Fplugins%252Flearnpress%252Fassets%252Fimages%26ID%3D4786%26CurrentPage%3D1"}/>
@@ -13,25 +19,23 @@ const NewsCard = ({newsItem}) => {
                     <span className = "author">
                         <a href = {newsItem.url} target = "__blank"><b>short</b></a>{" "}
                         <span className = "muted">
-                            {" "}by {newsItem.author ? newsItem.author:"unknown"} {" "} /
+                            {" "}by {newsItem.author ? newsItem.author:"unknown"} {" "} /  {" "}
+                            {time? `${hour-12}:${date[4].substring(3,5)} AM`:`${hour}:${date[4].substring(3,5)} PM`} {" "} on {date[2]} {date[1]} {date[3]}, {date[0]} 
                         </span>
                     </span>
                 </div>
                 <div className = "lowerText">
                     <div className = "description">{newsItem.description}</div>
-                    
-                </div>
-                <div className = "readMore">
+                    <div className = "readMore">
                     <span>
                         read more at {" "}
                         <a href = {newsItem.url} target = "__blank" className = "source">
                             <b>{newsItem.source.name}</b>
                         </a>
                     </span>
-                    </div>
-                
+                    </div>  
+                </div>
             </div>
-            
         </div>
     )
 }
